5626a2245ff28f55a3659bbbb0f4c1a99922308f
Iskander <nomail86@gmail.com>
2020-04-19 18:22:45 +0300

    Update UriExtensions.cs

--- src/Badger.Web/Processors/CertificateQuery/Extensions/UriExtensions.cs
+++ src/Badger.Web/Processors/CertificateQuery/Extensions/UriExtensions.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Linq.Expressions;
 using System.Net.Http;
-using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks;
 
 namespace Badger.Web.Processors.CertificateQuery.Extensions
 {
@@ -15,30 +12,13 @@ namespace Badger.Web.Processors.CertificateQuery.Extensions
             X509Certificate2 serverCertificate = null;
             handler.ServerCertificateCustomValidationCallback = (message, certificate, chain, policyErrors) =>
             {
-                policyErrors = System.Net.Security.SslPolicyErrors.None;
                 serverCertificate = certificate;
                 return true;
             };
 
             var httpClient = new HttpClient(handler);
-            try
-            {
-                var response = httpClient.GetAsync(uri).Result;
+            var response = httpClient.GetAsync(uri).Result;
 
-            }
-            catch(AuthenticationException)
-            {
-            }
-            //var client = new TcpClient(uri.Host, uri.Port);
-
-            //var sslStream = new SslStream(client.GetStream());
-            //await sslStream.AuthenticateAsClientAsync(new SslClientAuthenticationOptions
-            //{
-            //    TargetHost = uri.ToString(),
-            //    RemoteCertificateValidationCallback = (sender, x509Certificate, chain, errors) => true
-            //});
-
-            //var certificate = new X509Certificate2(sslStream.RemoteCertificate);
             return serverCertificate;
         }
     }
