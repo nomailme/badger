37dc485018e442eb0188104ccc6c2d27537cc24c
Iskander <nomail86@gmail.com>
2020-04-19 17:32:14 +0300

    Update UriExtensions.cs

--- src/Badger.Web/Processors/CertificateQuery/Extensions/UriExtensions.cs
+++ src/Badger.Web/Processors/CertificateQuery/Extensions/UriExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Net.Security;
-using System.Net.Sockets;
+using System.Net.Http;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
@@ -10,16 +9,28 @@ namespace Badger.Web.Processors.CertificateQuery.Extensions
     {
         public static async Task<X509Certificate2> GetCertificateAsync(this Uri uri)
         {
-            var client = new TcpClient(uri.Host, uri.Port);
-            var sslStream = new SslStream(client.GetStream());
-            await sslStream.AuthenticateAsClientAsync(new SslClientAuthenticationOptions
+            var handler = new HttpClientHandler();
+            X509Certificate2 serverCertificate = null;
+            handler.ServerCertificateCustomValidationCallback = (message, certificate, chain, policyErrors) =>
             {
-                TargetHost = uri.ToString(),
-                RemoteCertificateValidationCallback = (sender, x509Certificate, chain, errors) => true
-            });
+                serverCertificate = certificate;
+                return true;
+            };
 
-            var certificate = new X509Certificate2(sslStream.RemoteCertificate);
-            return certificate;
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(uri);
+
+            //var client = new TcpClient(uri.Host, uri.Port);
+
+            //var sslStream = new SslStream(client.GetStream());
+            //await sslStream.AuthenticateAsClientAsync(new SslClientAuthenticationOptions
+            //{
+            //    TargetHost = uri.ToString(),
+            //    RemoteCertificateValidationCallback = (sender, x509Certificate, chain, errors) => true
+            //});
+
+            //var certificate = new X509Certificate2(sslStream.RemoteCertificate);
+            return serverCertificate;
         }
     }
 }
